a183a20b1ea84de5da16d7a90ae847c794c15ebf
Diogo <DiogovBortolotti@gmail.com>
2022-06-26 13:27:15 -0300

    Return page / CSS

--- receitas/views.py
+++ receitas/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utilitarios.fake_info import criar_receita
 
 from .models import Receita
@@ -8,19 +8,34 @@ from .models import Receita
 # Create your views here.
 def home(request):
     receitas = Receita.objects.filter(
-        publicacao=True).order_by('-id')
-    return render(request, 'receitas/pages/home.html',
-                  context={'receitas': receitas})  # [criar_receita() for _ in range(100)], })
+        publicacao=True,).order_by('-id')
+    # receitas = Receita.objects.filter(
+     #   publicacao=True).order_by('-id')
+   # return render(request, 'receitas/pages/home.html',
+      #            context={'receitas': receitas})  # [criar_receita() for _ in range(100)], })
 
 
 def categoria(request, categoria_id):
-    receitas = Receita.objects.filter(
-        publicacao=True, categoria_id=categoria_id).order_by('-id')
+        receitas = get_list_or_404(
+        Receita.objects.filter(
+            publicacao=True, categoria_id=categoria_id).order_by('-id')
+    )
     return render(request, 'receitas/pages/categoria.html',
-                  context={'receitas': receitas})
+                  context={'receitas': receitas,
+                           'titulo': f'{receitas[0].categoria.nome} - Categoria'})
+    # receitas = Receita.objects.filter(
+    #  publicacao=True, categoria_id=categoria_id).order_by('-id')
+
+    # if not receitas:
+    #   raise Http404('Não Deveria Ocorrer')
+    # return HttpResponse(content='Opa Voce não deveria estár aqui', status=404)
+
 
 
 def receita(request, id):
+    receita=Receita.objects.filter(
+        publicacao=True, id=id).order_by('-id').first()
+
     return render(request, 'receitas/pages/receita.html',
-                  context={'receita': criar_receita(),
+                  context={'receita': receita,
                            'detail_page': True, })
