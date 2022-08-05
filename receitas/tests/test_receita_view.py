9763f0531dc2716f71fe2353afef90a569e2a239
Diogo <DiogovBortolotti@gmail.com>
2022-08-04 21:36:14 -0300

    teste e base de testes

--- receitas/tests/test_receita_view.py
+++ receitas/tests/test_receita_view.py
@@ -1,19 +1,17 @@
 
-# from django.contrib.auth.models import User
+# from django.contrib.auth.models import User  from django.test import TestCase
 
-from http.client import responses
-
-from django.test import TestCase
 from django.urls import resolve, reverse
 from receitas import views
-from receitas.models import Categoria, Receita, User
+
+from .teste_receita_base import ReceitaTesteBase
 
 # Create your tests here.
 # pip install pytest-watch
 # ptw
 
 
-class ReceitasViewsTest(TestCase):
+class ReceitasViewsTest(ReceitaTesteBase):
 
     def test_receitas_views_functions(self):
         view_home = resolve(reverse('receitas:home'))
@@ -45,43 +43,3 @@ class ReceitasViewsTest(TestCase):
 
 
 # com conteudo testes:
-
-
-    def test_receitas_home_template_loads_receitas(self):
-        categoria = Categoria.objects.create(nome='Categoria')
-        # categoria = Categoria(nome='Categoria')
-        # categoria.full_clean()
-        # categoria.save()
-        autor = User.objects.create_user(first_name='user',
-                                         last_name='name',
-                                         username='username',
-                                         password='123456',
-                                         email='username@example.com')
-
-        receita = Receita.objects.create(
-            categoria=categoria,
-            usuario=autor,
-            titulo='Receita Titulo',
-            descricao='Receita Descrição',
-            slug='receita-slug',
-            tempo_preparo=10,
-            tipo_tempo='Minutos',
-            quantidade_porcao=5,
-            tipo_unitario='Porções',
-            etapa_de_preparo='Etapa de Preparação',
-            etapa_de_preparo_e_html=False,
-            capa='colocar_algo_se_nao_da_erro_pois_nao_possui_imagem',
-            publicacao=True,
-        )
-        resposta = self.client.get(reverse('receitas:home'))
-        resposta_receitas = resposta.context['receitas']
-        self.assertEqual(resposta_receitas.first().titulo, 'Receita Titulo')
-
-        content = resposta.content.decode('utf-8')
-        self.assertIn('Receita Titulo', content)
-        self.assertIn('10 Minutos', content)
-        # categoria = Categoria(nome='Categoria')
-        # categoria.full_clean()
-        # categoria.save()
-
-        pass
